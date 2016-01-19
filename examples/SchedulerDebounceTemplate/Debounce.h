1c9088a317b2109743841b43ceb9d2a75a564968
mikaelpatel <mikael.patel@gmail.com>
2016-01-19 16:12:32 +0100

    New example sketch: template based debounce task class.

---
+++ examples/SchedulerDebounceTemplate/Debounce.h
@@ -0,0 +1,49 @@
+/**
+ * @file Debounce.h
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) Mikael Patel, 2015
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * @section Description
+ * Debounce task template class.
+ */
+
+#ifndef DEBOUNCE_H
+#define DEBOUNCE_H
+
+template<int PIN, void (*CALLBACK)()>
+class Debounce {
+public:
+
+  static void setup()
+  {
+    pinMode(PIN, INPUT_PULLUP);
+  }
+
+  static void loop()
+  {
+    const unsigned int DEBOUNCE = 50;
+    int state = digitalRead(PIN);
+    while (1) {
+      await(digitalRead(PIN) != state);
+      unsigned int start = millis();
+      if (state) CALLBACK();
+      unsigned int ms = millis() - start;
+      if (ms < DEBOUNCE) delay(DEBOUNCE - ms);
+      state = !state;
+    }
+  }
+};
+
+#endif
---
+++ examples/SchedulerDebounceTemplate/SchedulerDebounceTemplate.ino
@@ -0,0 +1,50 @@
+/**
+ * @file SchedulerDebounceTemplate.ino
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) Mikael Patel, 2015
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * @section Description
+ * This Arduino sketch shows how to use the Scheduler library;
+ * Rewrite of SchedulerDebounce with Debounce task template class.
+ */
+
+#include <Scheduler.h>
+#include "Debounce.h"
+
+unsigned int count = 0;
+
+void buttonDown()
+{
+  Serial.print(millis());
+  Serial.println(F(":buttonDown"));
+  count += 1;
+}
+
+Debounce<4, buttonDown> button;
+
+void setup()
+{
+  Serial.begin(57600);
+  Serial.println(F("SchedulerDebounceTemplate: started"));
+  Scheduler.start(button.setup, button.loop);
+}
+
+void loop()
+{
+  Serial.print(millis());
+  Serial.print(F(":loop::count="));
+  Serial.println(count);
+  delay(1000);
+}
