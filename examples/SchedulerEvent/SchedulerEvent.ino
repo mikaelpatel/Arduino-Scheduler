07549d0814418f15e734dd77bb28981b9993fc53
mikaelpatel <mikael.patel@gmail.com>
2016-01-21 19:50:57 +0100

    New example sketch; task events.

---
+++ examples/SchedulerEvent/SchedulerEvent.ino
@@ -0,0 +1,96 @@
+/**
+ * @file SchedulerEvent.ino
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) Mikael Patel, 2015
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * @section Description
+ * This Arduino sketch shows how to use the Scheduler library;
+ * Pass events between tasks.
+ */
+
+#include <Scheduler.h>
+
+class Event {
+public:
+  /**
+   * Initiate task event.
+   */
+  Event() : m_event(0) {}
+
+  /**
+   * Wait for task event. Returns event code (non zero).
+   * Typically called by event handler.
+   * @return event.
+   */
+  unsigned int wait()
+  {
+    await(m_event != 0);
+    return (m_event);
+  }
+
+  /**
+   * Signal task event. Wait for ongoing event handler,
+   * assign new event code and yield.
+   * @param[in] value event code (non zero).
+   */
+  void signal(unsigned int value)
+  {
+    await(m_event == 0);
+    m_event = value;
+    yield();
+  }
+
+  /**
+   * Clear event code to mark that it was completed.
+   */
+  void clear()
+  {
+    m_event = 0;
+  }
+
+protected:
+  volatile unsigned int m_event;
+};
+
+Event event;
+
+void setup()
+{
+  Serial.begin(57600);
+  Serial.println(F("SchedulerEvent: started"));
+  Scheduler.start(NULL, eventHandler);
+}
+
+void loop()
+{
+  static unsigned int i = 1;
+  Serial.print(millis());
+  Serial.print(F(":loop::i="));
+  Serial.println(i);
+  delay(1000);
+  event.signal(i);
+  i += 1;
+  if (i == 0) i = 1;
+}
+
+void eventHandler()
+{
+  unsigned int value = event.wait();
+  Serial.print(millis());
+  Serial.print(F(":eventHandler::event="));
+  Serial.println(value);
+  delay(200);
+  event.clear();
+}
