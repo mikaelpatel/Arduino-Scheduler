1f359d2f7b0465dda45d7df4f245e5f33871a13d
mikaelpatel <mikael.patel@gmail.com>
2016-01-22 12:28:27 +0100

    New example sketch; event queue template class.

---
+++ examples/SchedulerEventQueue/Queue.h
@@ -0,0 +1,105 @@
+/**
+ * @file Queue.h
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) 2012-2015, Mikael Patel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+/**
+ * Template class for ring-buffer of queueing data elements.
+ * @param[in] T element class.
+ * @param[in] nmemb number of elements in queue.
+ */
+template <class T, uint8_t NMEMB>
+class Queue {
+  static_assert(NMEMB && !(NMEMB & (NMEMB - 1)), "NMEMB should be power of 2");
+public:
+  /**
+   * Construct a ring-buffer queue with given number of members and
+   * member type.
+   */
+  Queue() :
+    m_put(0),
+    m_get(0)
+  {}
+
+  /**
+   * Return number of elements in queue.
+   * @return available elements.
+   */
+  unsigned int available() const
+    __attribute__((always_inline))
+  {
+    return ((NMEMB + m_put - m_get) & MASK);
+  }
+
+  /**
+   * Number of elements room in queue.
+   * @return room for elements.
+   */
+  unsigned int room() const
+    __attribute__((always_inline))
+  {
+    return ((NMEMB - m_put + m_get - 1) & MASK);
+  }
+
+  /**
+   * Push given data into queue. Will yield until room is available.
+   * @param[in] data pointer to data buffer.
+   */
+  void push(T* data)
+  {
+    while (!room()) yield();
+    unsigned int next = (m_put + 1) & MASK;
+    m_buffer[next] = *data;
+    m_put = next;
+  }
+
+  /**
+   * Push given data in program memory into queue. Will yield until
+   * room is available.
+   * @param[in] data pointer to data buffer in program memory.
+   */
+  void push_P(const T* data)
+  {
+    while (!room()) yield();
+    unsigned int next = (m_put + 1) & MASK;
+    memcpy_P(&m_buffer[next], data, sizeof(T));
+    m_put = next;
+  }
+
+  /**
+   * Pull data from queue to given buffer. Will yield until data is
+   * available.
+   * @param[in,out] data pointer to data buffer.
+   */
+  void pull(T* data)
+  {
+    while (!available()) yield();
+    unsigned int next = (m_get + 1) & MASK;
+    m_get = next;
+    *data = m_buffer[next];
+  }
+
+private:
+  static const unsigned int MASK = (NMEMB - 1);
+  volatile unsigned int m_put;
+  volatile unsigned int m_get;
+  T m_buffer[NMEMB];
+};
+
+#endif
---
+++ examples/SchedulerEventQueue/SchedulerEventQueue.ino
@@ -0,0 +1,67 @@
+/**
+ * @file SchedulerEventQueue.ino
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) 2015-2016, Mikael Patel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * @section Description
+ * This Arduino sketch shows how to use the Scheduler library;
+ * Queue events between tasks.
+ */
+
+#include <Scheduler.h>
+#include "Queue.h"
+
+Queue<unsigned int, 8> eventq;
+
+void setup()
+{
+  Serial.begin(57600);
+  Serial.println(F("SchedulerEventQueue: started"));
+  Serial.flush();
+
+  // Start two event handlers
+  Scheduler.start(NULL, eventHandler);
+  Scheduler.start(NULL, eventHandler);
+}
+
+void loop()
+{
+  // Push events
+  static unsigned int event = 0;
+  Serial.print(millis());
+  Serial.print(F(":loop::event="));
+  Serial.println(event);
+  eventq.push(&event);
+  event += 1;
+
+  // Random processing time
+  delay(random(500));
+}
+
+void eventHandler()
+{
+  // Pull events
+  unsigned int event;
+  eventq.pull(&event);
+  Serial.print(millis());
+  Serial.print(F(":eventHandler#"));
+  Serial.print((int) &event);
+  Serial.print(':');
+  Serial.print(F("::event="));
+  Serial.println(event);
+
+  // Random service time
+  delay(random(1000));
+}
