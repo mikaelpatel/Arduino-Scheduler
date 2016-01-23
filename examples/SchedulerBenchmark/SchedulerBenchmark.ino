8a1be847d2bc9db01a89fd7aa50f59d4885dbe01
mikaelpatel <mikael.patel@gmail.com>
2016-01-23 18:24:30 +0100

    Cleanup.

--- examples/SchedulerBenchmark/SchedulerBenchmark.ino
+++ examples/SchedulerBenchmark/SchedulerBenchmark.ino
@@ -1,56 +1,56 @@
 /**
-   @file SchedulerBenchmark.ino
-   @version 1.0
-
-   @section License
-   Copyright (C) 2015-2016, Mikael Patel
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
-
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-
-   @section Description
-   Benchmark Scheduler library.
-
-   @section Results Arduino 1.6.7, Arduino Pro-Mini
-   SchedulerBenchmark: started
-   1. Initiate scheduler and main task: 4 us
-   2. Yield main task: 11.00 us
-   3. Start a task: 16 us
-   4. Yield between two tasks: 22.89 us
-   5. Delay 10 ms and check increments: 10028:381, 26.32 us
-   6. Start 8 tasks: 140:8, 17.50 us
-   7. Yield and check increments: 120:9, 13.33 us
-   8. Delay 10 ms and check increments: 10084:747, 13.50 us
-
-   @section Results Arduino 1.6.7, Arduino Mega 2560
-   SchedulerBenchmark: started
-   1. Initiate scheduler and main task: 4 us
-   2. Yield main task: 12.64 us
-   3. Start a task: 20 us
-   4. Yield between two tasks: 26.16 us
-   5. Delay 10 ms and check increments: 10028:338, 29.67 us
-   6. Start 47 tasks: 824:47, 17.53 us
-   7. Yield and check increments: 660:48, 13.75 us
-   8. Delay 10 ms and check increments: 10656:768, 13.88 us
-
-   @section Results Arduino 1.6.7, Arduino Due
-    SchedulerBenchmark: started
-   1. Initiate scheduler and main task: 1 us
-   2. Yield main task: 1.36 us
-   3. Start a task: 4 us
-   4. Yield between two tasks: 2.92 us
-   5. Delay 10 ms and check increments: 9711:3133, 3.10 us
-   6. Start 51 tasks: 112:51, 2.20 us
-   7. Yield and check increments: 81:52, 1.56 us
-   8. Delay 10 ms and check increments: 9939:6292, 1.58 us
-*/
+ * @file SchedulerBenchmark.ino
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) 2015-2016, Mikael Patel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * @section Description
+ * Benchmark Scheduler library.
+ *
+ * @section Results Arduino 1.6.7, Arduino Pro-Mini
+ * SchedulerBenchmark: started
+ * 1. Initiate scheduler and main task: 4 us
+ * 2. Yield main task: 11.00 us
+ * 3. Start a task: 16 us
+ * 4. Yield between two tasks: 22.89 us
+ * 5. Delay 10 ms and check increments: 10028:381, 26.32 us
+ * 6. Start 8 tasks: 140:8, 17.50 us
+ * 7. Yield and check increments: 120:9, 13.33 us
+ * 8. Delay 10 ms and check increments: 10084:747, 13.50 us
+ *
+ * @section Results Arduino 1.6.7, Arduino Mega 2560
+ * SchedulerBenchmark: started
+ * 1. Initiate scheduler and main task: 4 us
+ * 2. Yield main task: 12.64 us
+ * 3. Start a task: 20 us
+ * 4. Yield between two tasks: 26.16 us
+ * 5. Delay 10 ms and check increments: 10028:338, 29.67 us
+ * 6. Start 47 tasks: 824:47, 17.53 us
+ * 7. Yield and check increments: 660:48, 13.75 us
+ * 8. Delay 10 ms and check increments: 10656:768, 13.88 us
+ *
+ * @section Results Arduino 1.6.7, Arduino Due
+ * SchedulerBenchmark: started
+ * 1. Initiate scheduler and main task: 1 us
+ * 2. Yield main task: 1.36 us
+ * 3. Start a task: 4 us
+ * 4. Yield between two tasks: 2.92 us
+ * 5. Delay 10 ms and check increments: 9711:3133, 3.10 us
+ * 6. Start 51 tasks: 112:51, 2.20 us
+ * 7. Yield and check increments: 81:52, 1.56 us
+ * 8. Delay 10 ms and check increments: 9939:6292, 1.58 us
+ */
 
 #include <Scheduler.h>
 
