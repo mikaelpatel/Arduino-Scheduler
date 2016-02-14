6e9c71c0d72be3001ecb3c710357117706fdbe00
mikaelpatel <mikael.patel@gmail.com>
2016-02-14 14:10:26 +0100

    Added support for Message Channels.

---
+++ Scheduler/Channel.h
@@ -0,0 +1,81 @@
+/**
+ * @file Scheduler/Channel.h
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) 2015-2016, Mikael Patel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
+#ifndef SCHEDULER_CHANNEL_H
+#define SCHEDULER_CHANNEL_H
+
+#include <Scheduler.h>
+
+class Channel {
+public:
+  /**
+   * Initiate channel.
+   */
+ Channel() :
+   m_buf(NULL),
+   m_max(0),
+   m_size(0),
+   m_tag(0),
+   m_cnt(0)
+  {}
+
+  /**
+   * Send given message, buffer and size, to receiving task. Wait
+   * until receiver task is ready before copying message to receiver
+   * buffer. Yields to allow receiver to continue. Returns message
+   * size if successful otherwise negative error code.
+   * @param[in] buf message buffer.
+   * @param[in] size message size.
+   * @return message size or negative error code.
+   */
+  int send(const void* buf, size_t size)
+  {
+    uint8_t tag = m_tag++;
+    await(tag == m_cnt && m_max > 0 && m_size == 0);
+    if (size > m_max) return (-1);
+    memcpy(m_buf, buf, size);
+    m_size = size;
+    return (size);
+  }
+
+  /**
+   * Receive message from channel. Wait for sender task to copy
+   * message into given receive buffer. Returns received message size.
+   * @param[in] buf message buffer.
+   * @param[in] size message size.
+   * @return received message size.
+   */
+  int recv(void* buf, size_t size)
+  {
+    m_buf = buf;
+    m_max = size;
+    m_size = 0;
+    await(m_size != 0);
+    m_cnt += 1;
+    return (m_size);
+  }
+
+protected:
+  void* m_buf;
+  volatile size_t m_max;
+  volatile size_t m_size;
+  volatile uint8_t m_tag;
+  volatile uint8_t m_cnt;
+};
+
+#endif
---
+++ examples/SchedulerChannel/SchedulerChannel.ino
@@ -0,0 +1,80 @@
+/**
+ * @file SchedulerChannel.ino
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) 2015-2016, Mikael Patel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * @section Description
+ * This Arduino sketch shows how to use the Scheduler library;
+ * Message channel between tasks; two senders and one receiver.
+ */
+
+#include <Scheduler.h>
+#include <Scheduler/Channel.h>
+
+Channel chan;
+
+void setup()
+{
+  Serial.begin(57600);
+  while (!Serial);
+  Serial.println(F("SchedulerChannel: started"));
+  Serial.flush();
+
+  Scheduler.start(NULL, messageSender);
+  Scheduler.start(NULL, messageSender);
+  Scheduler.start(NULL, messageReceiver);
+}
+
+void loop()
+{
+  static unsigned int count = 0;
+  Serial.print(millis());
+  Serial.print(F(":loop:count="));
+  Serial.println(count++);
+  unsigned long start = millis();
+  await(millis() - start >= 1000);
+}
+
+struct msg_t {
+  int ref;
+  unsigned int nr;
+};
+
+void messageSender()
+{
+  msg_t msg = { (int) &msg, 0 };
+  while (1) {
+    Serial.print(millis());
+    Serial.print(F(":messageSender#"));
+    Serial.print(msg.ref);
+    Serial.print(F("::send msg="));
+    Serial.println(msg.nr);
+    chan.send(&msg, sizeof(msg));
+    msg.nr += 1;
+    delay(random(500));
+  }
+}
+
+void messageReceiver()
+{
+  msg_t msg;
+  chan.recv(&msg, sizeof(msg));
+  Serial.print(millis());
+  Serial.print(F(":messageReceiver::receive msg="));
+  Serial.print(msg.ref);
+  Serial.print(':');
+  Serial.println(msg.nr);
+  delay(random(1000));
+}
