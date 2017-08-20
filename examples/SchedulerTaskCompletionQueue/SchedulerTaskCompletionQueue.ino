4e581815439c5a3d95ee4042d2c22cbc3e434a80
mikaelpatel <mikael.patel@gmail.com>
2017-08-20 11:09:21 +0200

    Cleanup

--- examples/SchedulerTaskCompletionQueue/SchedulerTaskCompletionQueue.ino
+++ examples/SchedulerTaskCompletionQueue/SchedulerTaskCompletionQueue.ino
@@ -18,7 +18,8 @@
  *
  * @section Description
  * This Arduino sketch shows how to use the Scheduler library;
- * Queue events between tasks.
+ * Worker Design Pattern; Functions (tasks) are queued and executed by
+ * work threads.
  */
 
 #include <Scheduler.h>
@@ -29,48 +30,47 @@
 #define Serial SerialUSB
 #endif
 
-Queue<SchedulerClass::func_t, 4> taskq;
+// Queue of tasks (function pointers)
+typedef SchedulerClass::func_t task_t;
+const unsigned int TASKQ_MAX = 8;
+Queue<task_t, TASKQ_MAX> taskq;
 
+// Example task: Calculate sum(i): i=0..100
 volatile unsigned int sum100;
-
-volatile unsigned int fact10;
 
 void sumTo100()
 {
   const unsigned int N = 100;
   sum100 = 0;
-  for (unsigned int i = 0; i != N+1; ++i) 
-  {
-	  sum100 += i;
-	  delay(random(10));
+  for (unsigned int i = 0; i <= N; i++) {
+    sum100 += i;
+    delay(random(200));
   }
 }
+
+// Example task2: Calculate fac(10)
+volatile unsigned long int fact10;
 
 void factTo10()
 {
   const unsigned int N = 10;
   fact10 = 1;
-  for (unsigned int i = 1; i != N+1 ; ++i) 
-  {
-	  fact10 *= i;
-	  delay(random(10));
+  for (unsigned int i = 1; i <= N; i++) {
+    fact10 *= i;
+    delay(random(2000));
   }
 }
 
+// Example task3: simple background task (push back example)
 void printAlive()
 {
   Serial.print(millis());
-  Serial.print(F(":printAlive::"));
-  Serial.println(F("alive..."));
-  yield();
-}
+  Serial.println(F(":printAlive::alive..."));
+  Serial.flush();
+  delay(1000);
 
-void printAliveLoop()
-{
-  Serial.print(millis());
-  Serial.print(F(":printLoop::"));
-  Serial.println(F("alive loop..."));
-  delay(500);
+  task_t task = printAlive;
+  taskq.push(&task);
 }
 
 void setup()
@@ -80,49 +80,47 @@ void setup()
   Serial.println(F("SchedulerTaskCompletionQueue: started"));
   Serial.flush();
 
-  SchedulerClass::func_t task;
+  // Start two worker
+  Scheduler.startLoop(worker);
+  Scheduler.startLoop(worker);
 
+  // Push tasks
+  task_t task;
   task = sumTo100;
   Serial.print(millis());
-  Serial.print(F(":setup::push task sumto100="));
+  Serial.print(F(":setup::push task @sumTo100 = "));
   Serial.println(int(&sumTo100));
   taskq.push(&task);
-  
+
   task = factTo10;
   Serial.print(millis());
-  Serial.print(F(":setup::push task factTo10="));
+  Serial.print(F(":setup::push task @factTo10 = "));
   Serial.println(int(&factTo10));
   taskq.push(&task);
-  
+
   task = printAlive;
   Serial.print(millis());
-  Serial.print(F(":setup::push task printAlive="));
+  Serial.print(F(":setup::push task @printAlive = "));
   Serial.println(int(&printAlive));
   taskq.push(&task);
 
-  // Start two event handlers
-  Scheduler.startLoop(taskRunner);
-  Scheduler.startLoop(taskRunner);
-  Scheduler.startLoop(printAliveLoop);
+  // Note: The printAlive task will start when either sumTo100 or
+  // factTo10 are completed as there are only two workers.
 }
 
 void loop()
 {
-  delay(1000);
-  
   Serial.print(millis());
-  Serial.print(F(":loop sum100="));
-  Serial.println(sum100);
-  
-  Serial.print(millis());
-  Serial.print(F(":loop fact10="));
+  Serial.print(F(":loop::sum100 = "));
+  Serial.print(sum100);
+  Serial.print(F(", fact10 = "));
   Serial.println(fact10);
+  delay(1000);
 }
 
-void taskRunner()
+void worker()
 {
-  // Pull events
-  SchedulerClass::func_t* task;
-  taskq.pull(task);
-  (*task)();
+  task_t task;
+  taskq.pull(&task);
+  task();
 }
